0d2ce56a344cf7cb96279ff2b3bcf2d5c844a299
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-23 21:06:31 -0300

    O inicio de uma programaçao

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NomeCompleto_AT_Etec_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string NomeDoUsuario; 
+
+        string SobreNomeDoUsuario;
+
+        string NomeCompleto;
+
+            Console.Write("Por Favor, digite seu nome: ");
+            NomeDoUsuario = Console.ReadLine();
+
+            Console.Write("Agora digite seu SobreNome: ");
+            SobreNomeDoUsuario = Console.ReadLine();
+
+            NomeCompleto = $"{NomeDoUsuario} {SobreNomeDoUsuario}";
+
+            Console.Write($"Obrigado, {NomeCompleto}!! Precione em alguma tecla para finalizar");
+
+
+
+
+        }
+    }
+}
